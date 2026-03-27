8f03bed6acf7006491fc2788015d68dbed7db6a5
Sammeta Nakshathra <sammetanakshatra701@gmail.com>
2026-03-27 23:34:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+ESP32_IP: "192.168.1.25",
+
+DATA_ENDPOINT: "/data",
+
+START_ENDPOINT: "/start",
+
+STOP_ENDPOINT: "/stop",
+
+HEALTH_ENDPOINT: "/health",
+
+UPDATE_INTERVAL: 2000,
+
+THRESHOLD: 300
+
+};
